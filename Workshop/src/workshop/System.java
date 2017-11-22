2dd4af066592e101d002450ee0bfa48b0bfc25cb
Niclas Johansen <niclas@coweb.dk>
2017-11-22 11:42:29 +0100

    Created main

--- Workshop/src/workshop/System.java
+++ Workshop/src/workshop/System.java
@@ -11,11 +11,4 @@ package workshop;
  */
 public class System {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
 }
