b27494f34dd25dbcc859ea5ea65c941f132f17ed
le-magnum <32192192+le-magnum@users.noreply.github.com>
2017-11-22 11:54:42 +0100

    sensor

--- Workshop/src/workshop/Sensor.java
+++ Workshop/src/workshop/Sensor.java
@@ -5,14 +5,23 @@
  */
 package workshop;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author niclasjohansen
  */
 public class Sensor {
     private int data;
-    
+    private ArrayList<Measurement> measurement = new ArrayList<>();
+            
     public int getData(int data){
         return this.data = data;     
     }
+
+    public ArrayList<Measurement> getMeasurement() {
+        return measurement;
+    }
+    
+    
 }
