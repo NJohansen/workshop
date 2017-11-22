0406927f3bfcf73daa8944fe2f7f7b5c5b9747c4
Niclas Johansen <niclas@coweb.dk>
2017-11-22 11:37:09 +0100

    Classes created

--- Workshop/src/workshop/Room.java
+++ Workshop/src/workshop/Room.java
@@ -9,13 +9,6 @@ package workshop;
  *
  * @author niclasjohansen
  */
-public class Workshop {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Room {
     
 }
