334351db6a1b83285ef31bba40e783a41c71804c
Niclas Johansen <niclas@coweb.dk>
2017-11-22 13:36:03 +0100

    added stuff

--- Workshop/src/workshop/Starter.java
+++ Workshop/src/workshop/Starter.java
@@ -23,14 +23,15 @@ public class Starter {
         system.addBuilding(new Building("Bygning 5"));
 
         for (int i = 0; i < system.getBuildings().size(); i++) {
-            System.out.println(system.getBuildings().get(i).getName());
+            System.out.println("Building name: " + system.getBuildings().get(i).getName());
+            System.out.println("Building location: " + system.getBuildings().get(i).getLocation());
         }
 
         Location location = system.getBuildings().get(0).getLocation();
         System.out.println(location);
 
         BuildingManager system2 = new BuildingManager();
-        Building building = new Building("joes");
+        Building building = new Building("Joes");
         system2.addBuilding(building);
         Location location2 = new Location("City of Soren");
         system2.getBuildings().get(0).setLocation(location2);
