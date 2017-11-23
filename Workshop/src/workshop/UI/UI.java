68b1ff6b1e54b4298d37458bd0872b6b39818fca
Niclas Johansen <niclas@coweb.dk>
2017-11-23 12:49:50 +0100

    added ui

--- Workshop/src/workshop/UI/UI.java
+++ Workshop/src/workshop/UI/UI.java
@@ -6,11 +6,9 @@
 package workshop.UI;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,18 +19,8 @@ public class UI extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         
         Scene scene = new Scene(root, 300, 250);
         
