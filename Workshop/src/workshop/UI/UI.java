0b14813b16a9503a1f6a1a9ec9c7806104ff294f
Niclas Johansen <niclas@coweb.dk>
2017-11-27 16:30:11 +0100

    m

--- Workshop/src/workshop/UI/UI.java
+++ Workshop/src/workshop/UI/UI.java
@@ -11,12 +11,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import workshop.acqaintance.IBusiness;
+import workshop.acqaintance.IUI;
 
 /**
  *
  * @author niclasjohansen
  */
-public class UI extends Application {
+public class UI extends Application implements IUI {
+    private static IBusiness business;
     
     @Override
     public void start(Stage primaryStage) throws IOException {
