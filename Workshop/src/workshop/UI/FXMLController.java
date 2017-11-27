0092bec1952407386c6cef82f6ee01fb520a96a8
Niclas Johansen <niclas@coweb.dk>
2017-11-27 14:38:16 +0100

    Logik

--- Workshop/src/workshop/UI/FXMLController.java
+++ Workshop/src/workshop/UI/FXMLController.java
@@ -13,7 +13,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
-import workshop.BuildingManager;
+import workshop.business.BuildingManager;
 
 /**
  * FXML Controller class
